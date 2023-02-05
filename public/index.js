e1a0fe4f8dee3a94f8499415772dd1ab26677b01
Bang <122675456+ginganotnigg@users.noreply.github.com>
2023-02-05 23:20:07 +0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -3,100 +3,7 @@
 var socket = io("http://localhost:5500");
 socket.emit("allUsers");
 
-// Player
-
-let players = [];
-class Player{
-    constructor(index, name){
-        this.index = index;
-        this.name = name;
-    }
-}
-
-const randomRole = () => {
-    if (temp === 0) {
-        return role[0];
-    }
-    else {
-        if (temp < 3) {
-            return role[1];
-        }
-        else return role[2];
-    }
-}
-
-const characterRole = () => {
-    roleMes.text(randomRole());
-    if (temp === 0) {
-        roleDesc.text("Nhiệm vụ của bạn là tìm ra Killer đang trà trộn trong đoàn, sống sót và chiến thắng cùng Hunter.\n Mỗi lượt săn, bạn biết được số người chơi chọn 4 loại kho báu.");
-    }
-    else {
-        if (temp <= killerNum) {
-            roleDesc.text("Nhiệm vụ của bạn là cố gắng sống sót và kiếm được nhiều tiền nhất.\n Mỗi lượt săn, bạn có thể giết 1 Hunter nếu chỉ có Hunter đó săn cùng kho báu với bạn.");
-        }
-        else roleDesc.text("Nhiệm vụ của bạn là tìm giết Killer đang trà trộn trong đoàn.\n Mỗi lượt săn, nếu bạn được nhiều tiền nhất, bạn sẽ nhận được sự chú ý từ các thành viên khác và Killer sẽ không thể giết bạn.");
-    }
-    roleChr.css('background-color', selectedColor);
-}
-
-const randomRoomID = () => {
-    return Math.floor(Math.random() * 90000) + 10000;
-}
-
-const chestList = (ul, n) => {
-    let chest;
-    for (let i = 0; i < n; i++){
-        chest = $('<li>').addClass('option').text(i + 1);
-        ul.append(chest);
-    }
-}
-
-const createChestLists = (n) => {
-    const list_60 = $('.game_screen .chests .c60');
-    const list_80 = $('.game_screen .chests .c80');
-    const list_120 = $('.game_screen .chests .c120');
-
-    const n120 = Math.floor(n / 6);
-    const n60 = Math.floor((n - 1 - n120) / 2);
-    const n80 = Math.floor((n - 2 - n120) / 2);
-    console.log(n - 2 - n120);
-    console.log(n80);
-
-    chestList(list_60, n60);
-    chestList(list_80, n80);
-    chestList(list_120, n120);
-}
-
-const playerJoin = (id, username, room) => {
-    const player = {id, username, room};
-    players.push(player);
-    return player;
-};
-
-const playerLeave = (id) => {
-    const index = players.findIndex(player => player.id === id);
-    if (index !== -1) {
-        return players.splice(index, 1)[0];
-    }
-} 
-
-const getCurrentPlayer = (id) => {
-    return players.find(player => player.id === id);
-}
-
-const getRoomUsers = (room) => {
-    return players.filter(player => player.room === room);
-}
-
-const getActiveRooms = (players) => {
-    return new Set(players.map(player => player.room));
-}
-
-const getActiveNames = (players) => {
-    return players.map(player => player.username);
-}
-
-// User events
+// User constants
 
 const initialScr = $('.initial_screen');
 const roomBtns = $('.initial_screen button');
@@ -110,6 +17,7 @@ const roomIDMessage = $('.restroom_screen h3');
 const leaveRoomBtn = $('.restroom_screen #leaveRoomBtn');
 const startGameBtn = $('.restroom_screen #startGameBtn');
 const player_list = $(".player_list #show_player_list");
+//const playerBoxes = $(".player_list #show_player_list .box");
 const colorOptns = $('.restroom_screen .color_options .option');
 const sliders = $('.restroom_screen .customize div');
 const ranges = $('.restroom_screen .customize div input');
@@ -126,112 +34,141 @@ const roleChr = $('.role_screen .character');
 const activateScr = $('.activate_screen');
 const voteScr = $('.vote_screen');
 
-let room_size = $(".player_list #show_player_list .box").length;
+let selectedColor;
+let playerBoxes = $(".player_list #show_player_list .box");
+let room_size = playerBoxes.length;
 let temp = Math.floor(Math.random() * room_size);
 const role = ["Adventurer", "Killer", "Hunter"];
 const killerNum = 2;
 
-let selectedColor = "#a9a9a9";
 
-$(window).on('mouseup', (e) => {
+$(window).on('mouseup', function(e) {
     const container = $('.activate_screen, .vote_screen');
     if (!container.is(e.target) && container.has(e.target).length === 0){
         container.fadeOut();
     }
 });
 
-leaveRoomBtn.on('click', () => {
-    initialScr.css('display', "flex");
-    restroomScr.css('display', "none");
-});
+// User events
 
-startGameBtn.on('click', () => {
-    const roomID = getCurrentPlayer(socket.id).room;
-    room_size = $(".player_list #show_player_list .box").length;
-    if (room_size < 6 || room_size > 12){
-        alert("Game should be started with around 6-12 players!!!");
-        return;
+let players = [];
+class Player{
+    constructor(index, name){
+        this.index = index;
+        this.name = name;
     }
-    socket.emit("startGame", roomID);
-});
-
-for (let i = 0; i < sliders.length; i++) {
-    let range = sliders.eq(i).find('input');
-    let val = sliders.eq(i).find('span');
-    val.text(range.val());
-    range.on('input', () => {
-        const roomID = getCurrentPlayer(socket.id).room;
-        socket.emit("customize", roomID, range.val(), i);
-    });
 }
 
-for (let i = 0; i < colorOptns.length; i++){
-    colorOptns.eq(i).on('click', () => {
-        const elem = $('.color_options .selected');
-        if (elem != null) elem.removeClass('selected');
-        colorOptns.eq(i).addClass('selected');
-        selectedColor = colorOptns.eq(i).css('background-color');
-    });
-}
-
-activateBtn.on('click', () => {
-    activateScr.css('display', 'flex');
-});
-
-voteBtn.on('click', () => {
-    voteScr.css('display', 'flex');
-});
-
-for (let i = 0; i < chests.length; i++) {
-    chests.eq(i).on('click', () => {
-        const elem = $('.chests .option.selected');
-        if (elem != null) elem.removeClass('selected');
-        chests.eq(i).addClass('selected');
-    });
-}
-
-// Socket events
-
-socket.on("allUsers", listUser => {
-    players = listUser;
-});
-
-socket.on("updateRoom", roomUsers => {
-    player_list.html(``);
-    roomUsers.forEach(player => {
-        const playerDiv = $('<div>').addClass('box');
-        const playerCol = $('<div>').addClass('color');
-        playerDiv.append(playerCol);
-        playerDiv.append(`<p>${player.username}</p>`);
-        player_list.append(playerDiv);
-        if (player.id === socket.id) {
-            $(".player_list #show_player_list .box:last-child p").css('font-weight', "bold");
+const randomRole = function() {
+    if (temp === 0) {
+        return role[0];
+    }
+    else {
+        if (temp < 3) {
+            return role[1];
         }
-    });
-});
-
-socket.on("startGame", () => {
-    room_size = $(".player_list #show_player_list .box").length;
-    temp = Math.floor(Math.random() * room_size);
-    gameScr.css('display', "grid");
-    restroomScr.css('display', "none");
-    roleScr.css('display', "flex");
-    roleName.text("Role: " + randomRole());
-    createChestLists(room_size);
-    if (temp > killerNum) {
-        activateBtn.hide();
+        else return role[2];
     }
-    setTimeout(() => {
-        roleScr.fadeOut();
-    }, 8000);
-    characterRole();
-});
+}
 
-socket.on("customize", (value, i) => {
-    sliders.eq(i).find('span').text(value);
-});
+const characterRole = function() {
+    roleMes.text(randomRole());
+    if (temp === 0) {
+        roleDesc.text("Nhiệm vụ của bạn là tìm ra Killer đang trà trộn trong đoàn, sống sót và chiến thắng cùng Hunter.\n Mỗi lượt săn, bạn biết được số người chơi chọn 4 loại kho báu.");
+    }
+    else {
+        if (temp <= killerNum) {
+            roleDesc.text("Nhiệm vụ của bạn là cố gắng sống sót và kiếm được nhiều tiền nhất.\n Mỗi lượt săn, bạn có thể giết 1 Hunter nếu chỉ có Hunter đó săn cùng kho báu với bạn.");
+        }
+        else roleDesc.text("Nhiệm vụ của bạn là tìm giết Killer đang trà trộn trong đoàn.\n Mỗi lượt săn, nếu bạn được nhiều tiền nhất, bạn sẽ nhận được sự chú ý từ các thành viên khác và Killer sẽ không thể giết bạn.");
+    }
+    roleChr.css('background-color', selectedColor);
+}
 
-newRoomBtn.on("click", () => {
+const randomRoomID = function() {
+    return Math.floor(Math.random() * 90000) + 10000;
+}
+
+const chestList = function(ul, n) {
+    let chest;
+    for (let i = 0; i < n; i++){
+        chest = $('<li>').addClass('option').text(i + 1);
+        ul.append(chest);
+    }
+}
+
+const createChestLists = function(n) {
+    const list_60 = $('.game_screen .chests .c60');
+    const list_80 = $('.game_screen .chests .c80');
+    const list_120 = $('.game_screen .chests .c120');
+
+    const n120 = Math.floor(n / 6);
+    const n60 = Math.floor((n - 1 - n120) / 2);
+    const n80 = Math.floor((n - 2 - n120) / 2);
+    console.log(n - 2 - n120);
+    console.log(n80);
+
+    chestList(list_60, n60);
+    chestList(list_80, n80);
+    chestList(list_120, n120);
+}
+
+const playerJoin = function(id, username, room) {
+    const player = {id, username, room};
+    players.push(player);
+    return player;
+};
+
+const playerLeave = function(id) {
+    const index = players.findIndex(player => player.id === id);
+    if (index !== -1) {
+        return players.splice(index, 1)[0];
+    }
+} 
+
+const getCurrentPlayer = function(id) {
+    return players.find(player => player.id === id);
+}
+
+const getRoomUsers = function(room) {
+    return players.filter(player => player.room === room);
+}
+
+const getActiveRooms = function() {
+    return new Set(players.map(player => player.room));
+}
+
+const getActiveNames = function() {
+    return players.map(player => player.username);
+}
+
+const getUsedColors = function() {
+    return players.map(player => player.colorID);
+}
+
+const getDefaultColor = function() {
+    for (let i = 0; i < colorOptns.length; i++) {
+        if (!colorOptns.eq(i).hasClass('chosen')){
+            return i;
+        }
+    }
+    return -1;
+}
+
+const getBoxIndex = function(player) {
+    for (let i = 0; i < playerBoxes.length; i++){
+        if (playerBoxes.eq(i).find('p').text() == player.username){
+            return i;
+        }
+    }
+    return -1;
+}
+
+/*
+* INITIAL SCREEN
+*/
+
+newRoomBtn.on("click", function() {
     // Check valid
     const usernames = getActiveNames(players);
     const rooms = getActiveRooms(players);
@@ -252,11 +189,17 @@ newRoomBtn.on("click", () => {
     const username = usernameInput.val();
     socket.emit("joinRoom", username, roomID);
     roomIDMessage.text("ID: " + roomID);
+    const index = getDefaultColor();
+    colorOptns.eq(index).addClass('selected');
+    let boxID = getBoxIndex(getCurrentPlayer(socket.id));
+    selectedColor = colorOptns.eq(index).css('background-color');
+    playerBoxes.eq(boxID).find('.color').css('background-color', selectedColor);
+    socket.emit("selected", roomID, index);
     restroomScr.css('display', "grid");
     initialScr.css('display', "none");
 });
 
-joinRoomBtn.on('click', () => {
+joinRoomBtn.on('click', function() {
     // Check valid
     const usernames = getActiveNames(players);
     const rooms = getActiveRooms(players);
@@ -282,8 +225,133 @@ joinRoomBtn.on('click', () => {
     const username = usernameInput.val();
     socket.emit("joinRoom", username, roomID);
     roomIDMessage.text("ID: " + roomID);
+    setTimeout(() => {
+        const index = getDefaultColor();
+        colorOptns.eq(index).addClass('selected');
+        let boxID = getBoxIndex(getCurrentPlayer(socket.id));
+        selectedColor = colorOptns.eq(index).css('background-color');
+        playerBoxes.eq(boxID).find('.color').css('background-color', selectedColor);
+        socket.emit("selected", roomID, index);
+    }, 200);
     ranges.hide();
     startGameBtn.hide();
     restroomScr.css('display', "grid");
     initialScr.css('display', "none");
 });
+
+/*
+* RESTROOM SCREEN
+*/
+
+leaveRoomBtn.on('click', function() {
+    initialScr.css('display', "flex");
+    restroomScr.css('display', "none");
+});
+
+startGameBtn.on('click', function() {
+    const roomID = getCurrentPlayer(socket.id).room;
+    room_size = playerBoxes.length;
+    if (room_size < 6 || room_size > 12){
+        alert("Game should be started with around 6-12 players!!!");
+        return;
+    }
+    socket.emit("startGame", roomID);
+});
+
+for (let i = 0; i < sliders.length; i++) {
+    let range = sliders.eq(i).find('input');
+    let val = sliders.eq(i).find('span');
+    val.text(range.val());
+    range.on('input', function() {
+        const roomID = getCurrentPlayer(socket.id).room;
+        socket.emit("customize", roomID, range.val(), i);
+    });
+}
+
+for (let i = 0; i < colorOptns.length; i++){
+    colorOptns.eq(i).on('click', function() {
+        const roomID = getCurrentPlayer(socket.id).room;
+        const elem = $('.color_options .selected');
+        let index = getBoxIndex(getCurrentPlayer(socket.id));
+        if (elem != null) elem.removeClass('selected');
+        colorOptns.eq(i).addClass('selected');
+        selectedColor = colorOptns.eq(i).css('background-color');
+        playerBoxes.eq(index).find('.color').css('background-color', selectedColor);
+        socket.emit("selected", roomID, i);
+    });
+}
+
+/*
+* GAME SCREEN
+*/
+
+activateBtn.on('click', function() {
+    activateScr.css('display', 'flex');
+});
+
+voteBtn.on('click', function() {
+    voteScr.css('display', 'flex');
+});
+
+for (let i = 0; i < chests.length; i++) {
+    chests.eq(i).on('click', function() {
+        const elem = $('.chests .option.selected');
+        if (elem != null) elem.removeClass('selected');
+        chests.eq(i).addClass('selected');
+    });
+}
+
+// Socket events
+
+socket.on("allUsers", listUser => {
+    players = listUser;
+});
+
+socket.on("updateUsers", roomUsers => {
+    player_list.html(``);
+    roomUsers.forEach(player => {
+        const playerDiv = $('<div>').addClass('box');
+        const playerCol = $('<div>').addClass('color');
+        playerDiv.append(playerCol);
+        playerDiv.append(`<p>${player.username}</p>`);
+        player_list.append(playerDiv);
+        if (player.id === socket.id) {
+            $(".player_list #show_player_list .box:last-child p").css('font-weight', "bold");
+            playerBoxes = $(".player_list #show_player_list .box");
+        }
+    });
+});
+
+socket.on("startGame", function() {
+    room_size = playerBoxes.length;
+    temp = Math.floor(Math.random() * room_size);
+    gameScr.css('display', "grid");
+    restroomScr.css('display', "none");
+    roleScr.css('display', "flex");
+    roleName.text("Role: " + randomRole());
+    createChestLists(room_size);
+    if (temp > killerNum) {
+        activateBtn.hide();
+    }
+    setTimeout(function() {
+        roleScr.fadeOut();
+    }, 8000);
+    characterRole();
+});
+
+socket.on("customize", function(value, i) {
+    sliders.eq(i).find('span').text(value);
+});
+
+socket.on("updateColors", function() {
+    const elem = $('.color_options .chosen');
+    if (elem != null) elem.removeClass('chosen');
+    for (let i = 0; i < players.length; i++){
+        if (players[i].id !== socket.id && players[i].colorID > -1){
+            colorOptns.eq(players[i].colorID).addClass('chosen');
+            const chosenColor = colorOptns.eq(players[i].colorID).css('background-color');
+            console.log(chosenColor);
+            playerBoxes.eq(i).find('.color').css('background-color', chosenColor);
+        }
+    };
+})
