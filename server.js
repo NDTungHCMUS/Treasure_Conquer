4def49e1c31f82504926ebad26d1d31dd5756608
Bang <122675456+ginganotnigg@users.noreply.github.com>
2023-02-05 23:21:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,56 +11,65 @@ const {Server} = require("socket.io");
 
 const io = new Server(server);
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
    //res.sendFile(__dirname +"/index.html");
 });
 
-const playerJoin = (id, username, room) => {
+const playerJoin = function(id, username, room) {
     const player = {id, username, room};
     players.push(player);
     return player;
 };
 
-const playerLeave = (id) => {
+const playerLeave = function(id) {
     const index = players.findIndex(player => player.id === id);
     if (index !== -1) {
         return players.splice(index, 1)[0];
     }
 } 
 
-const getCurrentPlayer = (id) => {
+const getCurrentPlayer = function(id) {
     return players.find(player => player.id === id);
 }
 
-const getRoomUsers = (room) => {
+const getRoomUsers = function(room) {
     return players.filter(player => player.room === room);
 }
 
-const getActiveRooms = () => {
+const getActiveRooms = function() {
     return new Set(players.map(player => player.room));
 }
     
 let players = [];
-io.on("connection", (socket) => {
-    socket.on("joinRoom", (username, roomID) => {
+players.map(player => player.colorID = -1);
+io.on("connection", function(socket) {
+    socket.on("joinRoom", function(username, roomID) {
         const player = playerJoin(socket.id, username, roomID);
         socket.join(player.room);
         io.emit("allUsers", players);
-        io.to(player.room).emit("updateRoom", getRoomUsers(player.room));
+        io.to(player.room).emit("updateUsers", getRoomUsers(player.room));
+        io.to(roomID).emit("updateColors");
     });
 
-    socket.on("allUsers", () => {
+    socket.on("allUsers", function() {
         socket.emit("allUsers", players);
     });
 
-    socket.on("customize", (roomID, value, index) => {
+    socket.on("customize", function(roomID, value, index) {
         io.to(roomID).emit("customize", value, index);
     });
 
-    socket.on("startGame", roomID => {
+    socket.on("selected", function(roomID, index) {
+        let player = getCurrentPlayer(socket.id);
+        player.colorID = index;
+        io.emit("allUsers", players);
+        io.to(roomID).emit("updateColors");
+    });
+
+    socket.on("startGame", function(roomID) {
         io.to(roomID).emit("startGame");
     });
 })
-server.listen(5500, () => {
+server.listen(5500, function() {
     console.log("listen 5500");
 });
