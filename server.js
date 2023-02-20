bfcf21afe6076e8f90b5a885546d1268e7d7a7a9
Bang <122675456+ginganotnigg@users.noreply.github.com>
2023-02-20 22:09:24 +0700

    Optimize code structure

--- server.js
+++ server.js
@@ -1,212 +1,211 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname+ "/public")));
+app.use(express.static(path.join(__dirname + "/public")));
 
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-
-const {Server} = require("socket.io");
-
+const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', function(req, res) {
-   //res.sendFile(__dirname +"/index.html");
+app.get("/", function (req, res) {
+    //res.sendFile(__dirname +"/index.html");
 });
 
-const playerJoin = function(id, username, room) {
-    const player = {id, username, room};
+/*
+* USERS HANDLING FUNCTION
+*/
+
+const playerJoin = function (id, username, room) {
+    const player = { id, username, room };
     players.push(player);
-    for (let i = 0; i < leavePlayers.length; i++){
-        if (leavePlayers[i].username == player.username){
+    for (let i = 0; i < leavePlayers.length; i++) {
+        if (leavePlayers[i].username == player.username) {
             leavePlayers.splice(i, 1);
         }
     }
     return player;
 };
 
-const playerLeave = function(index) {
+const playerLeave = function (index) {
     let player;
     if (index !== -1) {
-        player =  players.splice(index, 1)[0];
+        player = players.splice(index, 1)[0];
     }
     leavePlayers.push(player);
     return player;
-} 
+};
 
-const getCurrentPlayer = function(id) {
-    return players.find(player => player.id === id);
+const getCurrentPlayer = function (id) {
+    return players.find((player) => player.id === id);
+};
+
+const getLeavePlayer = function (id) {
+    return leavePlayers.find((player) => player.id === id);
+};
+
+/*
+* ROOM HANDLING FUNCTION
+*/
+
+const addRoom = function (roomID) {
+    const room = {'id': roomID, 'stats': [2, 5, 30, 90]};
+    rooms.push(room);
+    return room;
+};
+
+const removeRoom = function (roomID) { 
+    let room;
+    const index = rooms.findIndex(x => x.id === roomID);
+    if (index !== -1){
+        room = rooms.splice(index, 1)[0];
+    }
+    return room;
+};
+
+const getRoomUsers = function (room) {
+    return players.filter((player) => player.room === room);
+};
+
+const getActiveRooms = function () {
+    return new Set(players.map((player) => player.room));
+};
+
+const getPlayingRooms = function() {
+    return rooms.filter((room) => room.hasOwnProperty('chestList'));
 }
 
-const getLeavePlayer = function(id) {
-    return leavePlayers.find(player => player.id === id);
+const getChestHunters = function (roomID, i) {
+    return getRoomUsers(roomID).filter((player) => player.chestID === i);
+};
+
+const getCurrentRoom = function (roomID) {
+    return rooms.find((x) => x.id === roomID);
 }
 
-const getRoomUsers = function(room) {
-    return players.filter(player => player.room === room);
-}
-
-const getActiveRooms = function() {
-    return new Set(players.map(player => player.room));
-}
-
-const getChestHunters = function(roomID, i){
-    return getRoomUsers(roomID).filter(player => player.chestID === i);
-}
-
-const getGameStats = function(roomID){
-    return allGameStats.find(x => x.room === roomID).stats;
-}
-
-const randomRole = function(randomID, i) {
+const randomRole = function (randomID, roomID, i) {
     if (randomID[i] === 0) {
         return role[0];
-    }
-    else if (randomID[i] <= allGameStats[0]) {
+    } else if (randomID[i] <= getCurrentRoom(roomID).stats[0]) {
         return role[1];
-    }
-    else if (randomID[i] === allGameStats[0] + 1){
+    } else if (randomID[i] === getCurrentRoom(roomID).stats[0] + 1) {
         return role[2];
-    }
-    else return role[3];
-}
-    
-let players = [];
-let leavePlayers = [];
-let playingRooms = [];
-let allGameStats = [];
-const role = ["Captain", "Killer", "Blacksmith", "Pirate"];
+    } else return role[3];
+};
 
-//Room management
-let roomList = []
-class RoomData {
-    constructor(roomID, roomSize){
-        this.id = roomID;
-        this.size = roomSize;
-        this.chestList = [];
-        this.createChestList();
-    }
-    createChestList(){
-        const n120 = Math.floor((this.size+2)/7);
-        const n80 = Math.floor((this.size-1-n120)/2);
-        const n60 = Math.floor((this.size-2-n120)/2);
-        const nList = [n120, n80, n60, 2];
-        const posList = [[[12,30.5],[33,76.5]],[[23,77],[18.5,57],[12,17],[29,27]],[[8,28],[15,37],[23,55],[10,47]],[[33, 41],[23,21]]];
-        const valueList = [120,80,60,40];
-        const idList = ['c120', 'c080', 'c060', 'c040'];
-        for (let i=0; i<4;i++){
-            for (let j=0; j<nList[i]; j++){
-                this.chestList.push({
-                    id : idList[i] + String(j),
-                    value: valueList[i],
-                    position: posList[i][j],
-                    whoChoosen: []
-                });
-            }
+const createChestList = function(room) {
+    const room_size = getRoomUsers(room.id).length;
+    const n120 = Math.floor((room_size + 2) / 7);
+    const n80 = Math.floor((room_size - 2 - n120) / 2);
+    const n60 = Math.floor((room_size - 1 - n120) / 2);
+    const nList = [n120, n80, n60, 2];
+    const posList = [[[7.5, 40.2], [31, 68]], [[23, 62], [17, 8], [12, 17], [28, 27]], [[4, 26], [15, 37], [22, 55], [6, 53]], [[33, 41], [23, 21]]];
+    const valueList = [120, 80, 60, 40];
+    const idList = ["c120", "c080", "c060", "c040"];
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < nList[i]; j++) {
+            room.chestList.push({id: idList[i] + j.toString(), value: valueList[i], position: posList[i][j], hunters: []});
         }
     }
-    getChestById(id){
-        this.chestList.forEach(chest => {
-            if (chest.id = id) {
-                return chest
-            }
-        });
-    }
-}
+};
+
+let players = [];
+let leavePlayers = [];
+let rooms = [];
+const role = ["Captain", "Killer", "Blacksmith", "Pirate"];
+
 
 // Socket events
 
-io.on("connection", function(socket) {
-    socket.on("room:join", function(isHost, username, roomID) {
-        const player = playerJoin(socket.id, username, roomID);
-        socket.join(player.room);
-        if (isHost){
-            allGameStats.push({'room': roomID, 'stats': [2, 5, 30, 90]});
-        }
-        io.emit("state:allUsers", players, leavePlayers, playingRooms);
-        io.to(roomID).emit("room:listing", getRoomUsers(player.room));
-        io.to(roomID).emit("room:coloring", getRoomUsers(player.room));
-        io.to(roomID).emit("room:customize", getGameStats(player.room));
+io.on("connection", function (socket) {
+    socket.on("state:allUsers", function () {
+        socket.emit("state:allUsers", players, leavePlayers, getPlayingRooms());
     });
 
-    socket.on("room:leave", function(leaveIndex){
-        const player = playerLeave(leaveIndex);
-        socket.leave(player.room);
-        io.emit("state:allUsers", players, leavePlayers, playingRooms);
-        io.to(player.room).emit("room:listing", getRoomUsers(player.room));
-        io.to(player.room).emit("room:coloring", getRoomUsers(player.room));
-    });
-    
-    socket.on("state:allUsers", function() {
-        socket.emit("state:allUsers", players, leavePlayers, playingRooms);
-    });
-
-    socket.on("room:customize", function(roomID, stats) {
-        allGameStats.find(x => x.room === roomID).stats = stats;
-        io.to(roomID).emit("room:customize", getGameStats(roomID));
-    });
-
-    socket.on("room:chooseColor", function(roomID, index) {
-        let player = getCurrentPlayer(socket.id);
-        player.colorID = index;
-        io.emit("state:allUsers", players, leavePlayers, playingRooms);
-        io.to(roomID).emit("room:coloring", getRoomUsers(player.room));
-    });
-
-    socket.on("game:start", function(roomID, room_size) {
-        // Generate random permutation from 0 to n - 1
-        let temp = [...Array(room_size).keys()];
-        for (let i = room_size - 1; i >= 0; i--){
-            let j = Math.floor(Math.random() * (i + 1));
-            [temp[i], temp[j]] = [temp[j], temp[i]];
-        }
-        let roomUsers = getRoomUsers(roomID);
-        for (let i = 0; i < room_size; i++){
-            roomUsers[i].role = randomRole(temp, i);
-        }
-        playingRooms.push(roomID);
-
-        let roomData = new RoomData(roomID, room_size);
-        roomList.push(roomData);
-
-        io.emit("state:allUsers", players, leavePlayers, playingRooms);
-        let timer = getGameStats(roomID)[2] + 8;
-        setInterval(function(){    
-            io.to(roomID).emit("game:timing", timer);
-            if (timer > 0) timer--;
-        }, 1000);
-        io.to(roomID).emit("game:start", temp, roomUsers, roomData);
-        chestRooms = new Array(room_size).fill([]);
-        
-
-    });
-    
-    socket.on("disconnect", function(){
+    socket.on("disconnect", function () {
         let player;
-        let index = players.indexOf(getCurrentPlayer(socket.id));   
-        if (index != -1){
-            player =  players.splice(index, 1)[0];
+        let index = players.indexOf(getCurrentPlayer(socket.id));
+        if (index != -1) {
+            player = players.splice(index, 1)[0];
             let roomID = player.room;
-            socket.leave(roomID);     
+            socket.leave(roomID);
             io.to(roomID).emit("room:listing", getRoomUsers(roomID));
             io.to(roomID).emit("room:coloring", getRoomUsers(roomID));
-        }
-        else {
+        } else {
             index = leavePlayers.indexOf(getLeavePlayer(socket.id));
             player = leavePlayers.splice(index, 1)[0];
         }
         io.emit("state:allUsers", players, leavePlayers);
     });
 
-    socket.on("game:huntChest", function(roomID, id){
+    socket.on("room:join", function (isHost, username, roomID) {
+        const player = playerJoin(socket.id, username, roomID);
+        socket.join(player.room);
+        if (isHost) {
+            addRoom(roomID);
+        }
+        io.emit("state:allUsers", players, leavePlayers, getPlayingRooms());
+        io.to(roomID).emit("room:listing", getRoomUsers(player.room));
+        io.to(roomID).emit("room:coloring", getRoomUsers(player.room));
+        io.to(roomID).emit("room:customize", getCurrentRoom(player.room).stats);
+    });
+
+    socket.on("room:leave", function (leaveIndex) {
+        const player = playerLeave(leaveIndex);
+        socket.leave(player.room);
+        if (getRoomUsers(player.room).length === 0){
+            removeRoom(player.room);
+        }
+        io.emit("state:allUsers", players, leavePlayers, getPlayingRooms());
+        io.to(player.room).emit("room:listing", getRoomUsers(player.room));
+        io.to(player.room).emit("room:coloring", getRoomUsers(player.room));
+    });
+
+    socket.on("room:customize", function (roomID, stats) {
+        getCurrentRoom(roomID).stats = stats;
+        io.to(roomID).emit("room:customize", getCurrentRoom(roomID).stats);
+    });
+
+    socket.on("room:chooseColor", function (roomID, index) {
+        let player = getCurrentPlayer(socket.id);
+        player.colorID = index;
+        io.emit("state:allUsers", players, leavePlayers, getPlayingRooms());
+        io.to(roomID).emit("room:coloring", getRoomUsers(player.room));
+    });
+
+    socket.on("game:start", function (roomID, room_size) {
+        // Generate random permutation from 0 to n - 1
+        let temp = [...Array(room_size).keys()];
+        for (let i = room_size - 1; i >= 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [temp[i], temp[j]] = [temp[j], temp[i]];
+        }
+        let roomUsers = getRoomUsers(roomID);
+        for (let i = 0; i < room_size; i++) {
+            roomUsers[i].role = randomRole(temp, roomID, i);
+        }
+        const room =  getCurrentRoom(roomID);
+        room.chestList = [];
+        createChestList(room);
+
+        io.emit("state:allUsers", players, leavePlayers, getPlayingRooms());
+        let timer = getCurrentRoom(roomID).stats[2] + 7;
+        setInterval(function () {
+            io.to(roomID).emit("game:timing", timer);
+            if (timer > 0) timer--;
+        }, 1000);
+        io.to(roomID).emit("game:start", temp, roomUsers, room);
+    });
+
+    socket.on("game:huntChest", function (roomID, id) {
         const currentPlayer = getCurrentPlayer(socket.id);
         currentPlayer.chestID = id;
-        io.emit("state:allUsers", players, leavePlayers, playingRooms);
+        io.emit("state:allUsers", players, leavePlayers, getPlayingRooms());
         io.to(roomID).emit("game:huntChest", getChestHunters(roomID, id), id);
     });
-})
+});
 
-server.listen(5500, function() {
+server.listen(5500, function () {
     console.log("listen 5500");
 });
