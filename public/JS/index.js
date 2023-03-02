9a803293e40000844833a058e030f1cbe33991bb
tungkhongthichcode <21125171@student.hcmus.edu.vn>
2023-03-02 23:04:04 +0700

    Fix import files in index.js

--- public/JS/index.js
+++ public/JS/index.js
@@ -3,49 +3,54 @@
 var socket = io("http://localhost:5500");
 socket.emit("state:allUsers");
 
-// Import other js files
+/* 
+Import other js files
+*/
+
+// Audio
 
 $.getScript('JS/audio.js', function() {
-    console.log("audio ne");
 });
 
+// Variables and functions
+
 $.getScript("JS/utils.js", function(){
-
-    // User events
-
-    $(window).on('mouseup', function(e) {
-        const container = $('.activate_screen, #storyScreen, #settingScreen');
-        if (ssiBar.hasClass('on_screen')){ 
-            ssiBar.removeClass('on_screen');
-        }
-        if (!container.is(e.target) && container.has(e.target).length === 0){
-            container.fadeOut();
-        }
-    });
-
-    // Initial Screen
-    
-    $.getScript("JS/Screen/initialScr.js", function(){
-
-    });
-    
-    // Restroom Screen
-    
-    $.getScript("JS/Screen/restRoomScr.js", function(){
-    });
-
-    // Game Screen
-        
-    $.getScript("JS/Screen/gameScr.js", function(){
-    });    
-
-    // Vote Screen
-        
-    $.getScript("JS/Screen/voteScr.js", function(){
-    });    
-
-    // Socket Event
-            
-    $.getScript("JS/event.js", function(){
-    });      
 })
+
+// User events
+
+$(window).on('mouseup', function(e) {
+    const container = $('.activate_screen, #storyScreen, #settingScreen');
+    if (ssiBar.hasClass('on_screen')){ 
+        ssiBar.removeClass('on_screen');
+    }
+    if (!container.is(e.target) && container.has(e.target).length === 0){
+        container.fadeOut();
+    }
+});
+
+// Initial Screen
+
+$.getScript("JS/Screen/initialScr.js", function(){
+});
+
+// Restroom Screen
+
+$.getScript("JS/Screen/restRoomScr.js", function(){
+});
+
+// Game Screen
+    
+$.getScript("JS/Screen/gameScr.js", function(){
+});    
+
+// Vote Screen
+    
+$.getScript("JS/Screen/voteScr.js", function(){
+});    
+
+// Socket Event
+        
+$.getScript("JS/event.js", function(){
+});      
+
